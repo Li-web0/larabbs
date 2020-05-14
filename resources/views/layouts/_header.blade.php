c874ea2ec2cd4262c7ee0c70c1013983b448687b
Li Qiu <li.qiu@student.adelaide.edu.au>
2020-05-14 16:10:15 +0930

    基础页面结构

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <!-- Branding Image -->
     <a class="navbar-brand " href="{{ url('/') }}">
-      LaraBBS
+      BT Job board
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,6 +20,13 @@
         <!-- Authentication Links -->
         <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
         <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+        {{--<li class="nav-item"><a class="nav-link" href="#">Post jobs</a></li>--}}
+        {{--<li class="nav-item"><a class="nav-link" href="#">Job board</a></li>--}}
+        {{--<li class="nav-item"><a class="nav-link" href="#">Job I posted</a></li>--}}
+        {{--<li class="nav-item"><a class="nav-link" href="#">Jobs Awaiting Approval</a></li>--}}
+        {{--<li class="nav-item"><a class="nav-link" href="#">Archived Jobs</a></li>--}}
+
+
       </ul>
     </div>
   </div>
